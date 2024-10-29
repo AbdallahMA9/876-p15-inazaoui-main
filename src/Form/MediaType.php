e4acf33c4ede1ca9e41d676ebce8afe53ae009d0
Abdallah <abdallah.merabet@outlook.com>
2024-10-29 13:53:22 +0100

    la vérification des images uploadés

--- src/Form/MediaType.php
+++ src/Form/MediaType.php
@@ -11,14 +11,23 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class MediaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+
             ->add('file', FileType::class, [
                 'label' => 'Image',
+                'required' => true,
+                'constraints' => [
+                    new Assert\Image([
+                        'maxSize' => '2M',
+                        'maxSizeMessage' => 'Le fichier ne doit pas dépasser 2 Mo.'
+                    ])
+                ],
             ])
             ->add('title', TextType::class, [
                 'label' => 'Titre',
