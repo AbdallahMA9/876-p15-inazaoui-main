238b5b53bb13538ab30e488d7041cc86baabb071
Abdallah <abdallah.merabet@outlook.com>
2024-11-11 09:38:09 +0100

    suppresion d'album qui entraine la suppression des medias associé

--- src/Entity/Album.php
+++ src/Entity/Album.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AlbumRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AlbumRepository::class)]
@@ -15,6 +17,14 @@ class Album
 
     #[ORM\Column]
     private string $name;
+
+    #[ORM\OneToMany(targetEntity: Media::class, mappedBy: 'album', cascade: ['remove'], orphanRemoval: true)]
+    private Collection $medias;
+
+    public function __construct()
+    {
+        $this->medias = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,5 +39,27 @@ class Album
     public function setName(string $name): void
     {
         $this->name = $name;
+    }
+
+    public function getMedias(): Collection
+    {
+        return $this->medias;
+    }
+
+    public function addMedia(Media $media): void
+    {
+        if (!$this->medias->contains($media)) {
+            $this->medias[] = $media;
+            $media->setAlbum($this);
+        }
+    }
+
+    public function removeMedia(Media $media): void
+    {
+        if ($this->medias->removeElement($media)) {
+            if ($media->getAlbum() === $this) {
+                $media->setAlbum(null);
+            }
+        }
     }
 }
